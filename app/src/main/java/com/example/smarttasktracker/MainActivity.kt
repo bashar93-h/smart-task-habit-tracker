21bb70b99f374b1373e027e69ccbdfdc25fbac86
Bashar <basharabulaziz25@gmail.com>
2026-03-15 10:01:28 +0300

    Setup Clean Architecture project structure

--- app/src/main/java/com/example/smarttasktracker/MainActivity.kt
+++ app/src/main/java/com/example/smarttasktracker/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -22,28 +23,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             SmartTaskTrackerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Surface(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {}
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SmartTaskTrackerTheme {
-        Greeting("Android")
-    }
-}
