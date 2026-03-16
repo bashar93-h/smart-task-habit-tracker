d8d688d6c7a656591f19e4b5dcdb87e212919405
Bashar <basharabulaziz25@gmail.com>
2026-03-16 23:35:43 +0300

    Add splash screen UI and move colors to theme

--- app/src/main/java/com/example/smarttasktracker/presentation/theme/Color.kt
+++ app/src/main/java/com/example/smarttasktracker/presentation/theme/Color.kt
@@ -9,3 +9,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val Background = Color(0xFFFDE8F5)
+val PrimaryPink = Color(0xFFE8709A)
+val TextMuted = Color(0xFFB89AB5)
+val BadgePinkBg = Color(0xFFFDDEED);
+val BadgePinkTx = Color(0xFFD0568A)
+val BadgeBlueBg = Color(0xFFDCF0FC);
+val BadgeBlueTx = Color(0xFF4A8DBF)
+val BadgeGreenBg = Color(0xFFD8F5E4);
+val BadgeGreenTx = Color(0xFF3A9E68)
