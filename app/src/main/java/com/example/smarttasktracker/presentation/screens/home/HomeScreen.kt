003012f5ee7c100d08903ba694b16a8f25eb6a6f
Bashar <basharabulaziz25@gmail.com>
2026-03-19 12:23:47 +0300

    feat: setup home screen scaffold with nav host and placeholder screens

--- app/src/main/java/com/example/smarttasktracker/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/smarttasktracker/presentation/screens/home/HomeScreen.kt
@@ -1,16 +1,24 @@
 package com.example.smarttasktracker.presentation.screens.home
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.smarttasktracker.presentation.components.AppBottomBar
+import com.example.smarttasktracker.presentation.components.AppTopBar
 
 @Composable
-fun HomeScreen(navController: NavController) {
-    Surface(modifier = Modifier.padding(1.dp)) {
-        Text("Home Screen")
+fun HomeScreen(navController: NavController?) {
+    Scaffold(topBar = {
+        AppTopBar("Smart Tracker", onMenuClick = {}, onQuoteClick = {})
+    }, bottomBar = { AppBottomBar(navController) }) { innerPadding ->
+        Surface(modifier = Modifier.padding(innerPadding)) { }
     }
 }
+
+@Preview
+@Composable
+fun HomeScreenPreview() = HomeScreen(null)
