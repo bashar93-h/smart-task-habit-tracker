682576364df96242558c77f597a75e8c254d4267
Bashar <basharabulaziz25@gmail.com>
2026-03-30 19:10:28 +0300

    feat: secondary screens — about, motivation, notification, saved quotes, settings, statistics

--- app/src/main/java/com/example/smarttasktracker/presentation/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/smarttasktracker/presentation/screens/settings/SettingsScreen.kt
@@ -1,16 +1,30 @@
 package com.example.smarttasktracker.presentation.screens.settings
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import com.example.smarttasktracker.presentation.components.AppBottomBar
+import com.example.smarttasktracker.presentation.components.AppTopBar
+import com.example.smarttasktracker.presentation.theme.SmartTaskTrackerTheme
 
 @Composable
-fun SettingsScreen(navController: NavController) {
-    Surface(modifier = Modifier.padding(1.dp)) {
-        Text("Home Screen")
+fun SettingsScreen(navController: NavController?) {
+    Scaffold(topBar = {
+        AppTopBar("Settings", onBackClick = { navController?.popBackStack() })
+    }, bottomBar = { AppBottomBar(navController) }) { innerPadding ->
+        Surface(modifier = Modifier.padding(innerPadding)) { }
+    }
+}
+
+
+@Preview
+@Composable
+fun SettingsScreenPreview() {
+    SmartTaskTrackerTheme() {
+        SettingsScreen(null)
     }
 }
